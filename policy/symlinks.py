be718d7ff5e59ba76b71776ea74a2603e0cded5b
Michael K. Johnson <http://bugs.rpath.com>
2006-04-19 11:43:38 -0400

    DanglingSymlinks is no reports all symbolic links as dangling symlinks if the buildpath contains a symbolic link (bz #856).

--- policy/symlinks.py
+++ policy/symlinks.py
@@ -108,67 +108,69 @@ class DanglingSymlinks(policy.PackagePolicy):
 	policy.PackagePolicy.doProcess(self, recipe)
 
     def doFile(self, path):
-	d = self.macros.destdir
-        l = len(d)
-	f = util.joinPaths(d, path)
+        d = self.macros.destdir
+        f = util.joinPaths(d, path)
+        if not os.path.islink(f):
+            return
+
         recipe = self.recipe
-	if os.path.islink(f):
-	    contents = os.readlink(f)
-	    if contents[0] == '/':
-                self.warn('Absolute symlink %s points to %s,'
-                          ' should probably be relative', path, contents)
-		return
-	    abscontents = util.joinPaths(os.path.dirname(path), contents)
-            # now resolve any intermediate symlinks
-            abscontents = os.path.realpath(d+abscontents)[l:]
-            ap = recipe.autopkg
-	    if abscontents in ap.pathMap:
-		componentMap = ap.componentMap
-		if componentMap[abscontents] != componentMap[path] and \
-		   not path.endswith('.so') and \
-		   not componentMap[path].getName().endswith(':test'):
-		    # warn about suspicious cross-component symlink
-                    fromPkg = ap.componentMap[path]
-                    targetPkg = ap.componentMap[abscontents]
+        contents = os.readlink(f)
+        if contents[0] == '/':
+            self.warn('Absolute symlink %s points to %s,'
+                      ' should probably be relative', path, contents)
+	    return
+        abscontents = util.joinPaths(os.path.dirname(path), contents)
+        # now resolve any intermediate symlinks
+        dl = len(os.path.realpath(d))
+        abscontents = os.path.realpath(d+abscontents)[dl:]
+        ap = recipe.autopkg
+        if abscontents in ap.pathMap:
+	    componentMap = ap.componentMap
+	    if componentMap[abscontents] != componentMap[path] and \
+	       not path.endswith('.so') and \
+	       not componentMap[path].getName().endswith(':test'):
+	        # warn about suspicious cross-component symlink
+                fromPkg = ap.componentMap[path]
+                targetPkg = ap.componentMap[abscontents]
 
-                    found = False
-                    for depClass, dep in fromPkg.requires.iterDeps():
-                        d = deps.DependencySet()
-                        d.addDep(depClass, dep)
-                        if targetPkg.provides.satisfies(d):
-                            found = True
-                            break
+                found = False
+                for depClass, dep in fromPkg.requires.iterDeps():
+                    d = deps.DependencySet()
+                    d.addDep(depClass, dep)
+                    if targetPkg.provides.satisfies(d):
+                        found = True
+                        break
 
-                    if not found:
-                        self.warn('symlink %s points from package %s to %s',
-                                  path, componentMap[path].getName(),
-                                  componentMap[abscontents].getName())
-	    else:
-		for targetFilter, requirement in self.targetFilters:
-		    if targetFilter.match(abscontents):
-			# contents are an exception
-                        self.info('allowing special dangling symlink %s -> %s',
-                                  path, contents)
-                        if requirement:
-                            self.info('automatically adding requirement'
-                                      ' %s for symlink %s', requirement, path)
-                            # Requires has already run, touch this up
-                            pkg = ap.componentMap[path]
-                            if path not in pkg.requiresMap:
-                                pkg.requiresMap[path] = deps.DependencySet()
-                            pkg.requiresMap[path].addDep(
-                                deps.TroveDependencies,
-                                deps.Dependency(requirement, []))
-                            f = pkg.getFile(path)
-                            f.requires.set(pkg.requiresMap[path])
-                            pkg.requires.union(f.requires())
-			return
-                for pathName in recipe.autopkg.pathMap:
-                    if pathName.startswith(abscontents):
-                        # a link to a subdirectory of a file that is
-                        # packaged is still OK; this test is expensive
-                        # and almost never needed, so put off till last
-                        return
-		self.error(
-		    "Dangling symlink: %s points to non-existant %s (%s)"
-		    %(path, contents, abscontents))
+                if not found:
+                    self.warn('symlink %s points from package %s to %s',
+                              path, componentMap[path].getName(),
+                              componentMap[abscontents].getName())
+        else:
+	    for targetFilter, requirement in self.targetFilters:
+	        if targetFilter.match(abscontents):
+		    # contents are an exception
+                    self.info('allowing special dangling symlink %s -> %s',
+                              path, contents)
+                    if requirement:
+                        self.info('automatically adding requirement'
+                                  ' %s for symlink %s', requirement, path)
+                        # Requires has already run, touch this up
+                        pkg = ap.componentMap[path]
+                        if path not in pkg.requiresMap:
+                            pkg.requiresMap[path] = deps.DependencySet()
+                        pkg.requiresMap[path].addDep(
+                            deps.TroveDependencies,
+                            deps.Dependency(requirement, []))
+                        f = pkg.getFile(path)
+                        f.requires.set(pkg.requiresMap[path])
+                        pkg.requires.union(f.requires())
+		    return
+            for pathName in recipe.autopkg.pathMap:
+                if pathName.startswith(abscontents):
+                    # a link to a subdirectory of a file that is
+                    # packaged is still OK; this test is expensive
+                    # and almost never needed, so put off till last
+                    return
+	    self.error(
+	        "Dangling symlink: %s points to non-existant %s (%s)"
+	        %(path, contents, abscontents))
